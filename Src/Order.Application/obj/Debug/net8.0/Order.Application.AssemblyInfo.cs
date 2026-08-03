a370abb246175032dce6bf74f74651155205de9f
Roshan Sonekar <roshansonekar@gmail.com>
2026-08-03 10:27:51 +0200

    updated order.service

--- Src/Order.Application/obj/Debug/net8.0/Order.Application.AssemblyInfo.cs
+++ Src/Order.Application/obj/Debug/net8.0/Order.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Order.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5f598bbea62c9af19e0d8fa5f4e344b82cb8a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13025eed668bc07f9063abaf96c447bb04baf260")]
 [assembly: System.Reflection.AssemblyProductAttribute("Order.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Order.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Src/Order.Domain/obj/Debug/net8.0/Order.Domain.AssemblyInfo.cs
+++ Src/Order.Domain/obj/Debug/net8.0/Order.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Order.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5f598bbea62c9af19e0d8fa5f4e344b82cb8a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13025eed668bc07f9063abaf96c447bb04baf260")]
 [assembly: System.Reflection.AssemblyProductAttribute("Order.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Order.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
